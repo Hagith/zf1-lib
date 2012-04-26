20de9b5a5d3a56a1dae036886925023536f1ff7e
Rafal Galka <rafal@modernweb.pl>
2012-04-26 20:40:30 +0200

    version class

---
+++ library/Modern/Version.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * ModernWeb
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.modernweb.pl/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to kontakt@modernweb.pl so we can send you a copy immediately.
+ *
+ * @category    Modern
+ * @package     Modern_Version
+ * @author      Rafał Gałka <rafal@modernweb.pl>
+ * @copyright   Copyright (c) 2007-2012 ModernWeb (http://www.modernweb.pl)
+ * @license     http://www.modernweb.pl/license/new-bsd     New BSD License
+ */
+
+/**
+ * Class to store and retrieve the version of Modern libraries.
+ *
+ * @category    Modern
+ * @package     Modern_Version
+ * @author      Rafał Gałka <rafal@modernweb.pl>
+ * @copyright   Copyright (c) 2007-2012 ModernWeb (http://www.modernweb.pl)
+ */
+final class Modern_Version
+{
+    /**
+     * Modern libraries version identification - see compareVersion()
+     */
+    const VERSION = '0.1.0';
+
+    /**
+     * Compare the specified ModernApp version string $version
+     * with the current Modern_Version::VERSION of Modern libraries.
+     *
+     * @param  string  $version  A version string (e.g. "0.7.1").
+     * @return int               -1 if the $version is older,
+     *                           0 if they are the same,
+     *                           and +1 if $version is newer.
+     *
+     */
+    public static function compareVersion($version)
+    {
+        $version = strtolower($version);
+        $version = preg_replace('/(\d)pr(\d?)/', '$1a$2', $version);
+        return version_compare($version, strtolower(self::VERSION));
+    }
+
+}
