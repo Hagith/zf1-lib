4a07ce80928149c5d80dd7075d71e92a8abf8ad7
Rafal Galka <rafal@modernweb.pl>
2012-04-26 22:09:11 +0200

    Modern_String

---
+++ library/Modern/String/Exception.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * ModernWeb
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.modernweb.pl/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to kontakt@modernweb.pl so we can send you a copy immediately.
+ *
+ * @category    Modern
+ * @package     Modern_String
+ * @author      Rafał Gałka <rafal@modernweb.pl>
+ * @copyright   Copyright (c) 2007-2012 ModernWeb (http://www.modernweb.pl)
+ * @license     http://www.modernweb.pl/license/new-bsd     New BSD License
+ */
+
+/** @see Modern_Exception */
+require_once('Modern/Exception.php');
+
+/**
+ * @category    Modern
+ * @package     Modern_String
+ * @author      Rafał Gałka <rafal@modernweb.pl>
+ * @copyright   Copyright (c) 2007-2012 ModernWeb (http://www.modernweb.pl)
+ */
+class Modern_String_Exception extends Modern_Exception
+{
+}
